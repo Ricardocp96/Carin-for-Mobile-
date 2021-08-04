d3be4081674a87309c10371e9ccfcb7a772a02b8
Ricardocp96 <richardlubanga@outlook.com>
2021-08-04 22:06:26 +0800

    Firabes config files

---
+++ Firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyATWxamZRMUX60t0EVK8qmDZdkr9gchJvQ",
+    authDomain: "carin-f2cdf.firebaseapp.com",
+    projectId: "carin-f2cdf",
+    storageBucket: "carin-f2cdf.appspot.com",
+    messagingSenderId: "809444901806",
+    appId: "1:809444901806:web:bf7dd173e9b28b14513e3f",
+    measurementId: "G-M8VW8YVWPZ"
+  };
